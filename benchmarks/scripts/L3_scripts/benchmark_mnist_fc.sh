dcf842936e0451b88305258bbce52175cb0183f7
Max <max@dhcp-10-250-27-41.harvard.edu>
2019-10-21 16:35:13 -0400

    Large mnist script

--- benchmarks/scripts/L3_scripts/benchmark_mnist_fc.sh
+++ benchmarks/scripts/L3_scripts/benchmark_mnist_fc.sh
@@ -16,24 +16,20 @@ mbed new .
 cp ../artifacts/release.json .
 popd
 
-#h1_size=( 32 128 256 512 1024 )
-#h2_size=( 32 128 256 512 1024 )
-
-h1_size=( 8 16 32 64 128 )
-h2_size=( 8 16 32 64 128 )
+h1_size=( 8 16 32 64 128 256 512 1024 )
+h2_size=( 8 16 32 64 128 256 512 1024 )
 
 # Generate directories for 10, 100, 1000, 10000, 100000 byte read/write
-#python3 benchmarks/src/generate.py --tier L3 --task MnistFC --output-path ${generate_dir} --h1_size "[32,128,256,512,1024]" --h2_size "[32,128,256,512,1024]"
-python3 benchmarks/src/generate.py --tier L3 --task MnistFC --output-path ${generate_dir} --h1_size "[8, 16, 32, 64, 128]" --h2_size "[8, 16, 32, 64, 128]"
+python3 benchmarks/src/generate.py --tier L3 --task MnistFC --output-path ${generate_dir} --h1_size "[8,16,32,64,128,256,512,1024]" --h2_size "[8,16,32,64,128,256,512,1024]"
 
-for ((ii = 0; ii < ${#h1_size[@]}; ++ii)); do
-    i="${h1_size[$ii]}"
-    j="${h2_size[$ii]}"
-    # Compile and run
-    dirpath="${generate_dir}/MnistFC/h1_size=${i}_h2_size=${j}/"
-    echo "python3 benchmarks/scripts/compile_programs.py --target ${dirpath} --mbed-program-dir ${compile_dir}"
-    python3 benchmarks/scripts/compile_programs.py --target ${dirpath} --mbed-program-dir ${compile_dir}
-    python3 benchmarks/scripts/run_on_mcu.py --target ${dirpath} --timelimit 30 --output_path ${dirpath}/run_output
-    tail -n 1 ${dirpath}/run_output > ${dirpath}/stats.json
-    cp ${dirpath}/stats.json ${outpath}/MnistFCTask_h1_size=${i}_h2_size=${j}.json
+for i in "${h1_size[@]}"; do
+    for j in "${h2_size[@]}"; do
+        # Compile and run
+        dirpath="${generate_dir}/MnistFC/h1_size=${i}_h2_size=${j}/"
+        echo "python3 benchmarks/scripts/compile_programs.py --target ${dirpath} --mbed-program-dir ${compile_dir}"
+        python3 benchmarks/scripts/compile_programs.py --target ${dirpath} --mbed-program-dir ${compile_dir}
+        python3 benchmarks/scripts/run_on_mcu.py --target ${dirpath} --timelimit 30 --output_path ${dirpath}/run_output
+        tail -n 1 ${dirpath}/run_output > ${dirpath}/stats.json
+        cp ${dirpath}/stats.json ${outpath}/MnistFCTask_h1_size=${i}_h2_size=${j}.json
+    done
 done
